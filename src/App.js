ef31561ef192969f4583b96dd1732f49cb45275e
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-12-01 12:18:48 -0500

    Removed search bar component, changed handle search to search immediately with every change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React, { useState } from 'react';
 import EmployeeList from './components/EmployeeList'
+import SearchBar from './components/SearchBar'
 
 function App() {
   const [query, setQuery] = useState("");
 
-  function SearchBar(props) {
-    return (
-      <nav className="navbar navbar-light bg-info">
-        <form className="form-inline" onSubmit={props.handleSearch}>
-          <input className="form-control mr-sm-2" name="search" type="search" placeholder="Search Directory" aria-label="Search" />
-          <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-        </form>
-      </nav>
-    )
-  }
-  
   function handleSearch (event) {
     event.preventDefault();
-    setQuery(event.target.search.value.trim());
+    setQuery(event.target.value.trim());
   }
   
   return (
