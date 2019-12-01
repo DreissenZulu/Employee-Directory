721dc55932fc76d1a0043e6a4421408c4c355016
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-11-30 21:08:37 -0500

    Adjusted table formatting and information

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -6,14 +6,13 @@ function EmployeeInfo(props) {
         <div className="container">
             {props.employeeList.map(employee => (
                 <div className="row d-flex align-items-center">
-                    <div className="col-lg-1"><img src={employee.picture.medium} alt="Emp Img"/></div>
-                    <div className="col-lg-1">{employee.name.first}</div>
-                    <div className="col-lg-1">{employee.name.last}</div>
-                    <div className="col-lg-1">{employee.dob.date.split("T")[0]}</div>
-                    <div className="col-lg-2">{employee.location.city}</div>
-                    <div className="col-lg-2">{employee.location.state}</div>
-                    <div className="col-lg-3">{employee.email}</div>
-                    <div className="col-lg-1">{employee.phone}</div>
+                    <div className="col-md-1"><img src={employee.picture.medium} alt="Emp Img"/></div>
+                    <div className="col-md-1">{employee.name.first}</div>
+                    <div className="col-md-1">{employee.name.last}</div>
+                    <div className="col-md-2">{employee.dob.date.split("T")[0]}</div>
+                    <div className="col-md-2">{employee.location.city}</div>
+                    <div className="col-md-2">{employee.phone}</div>
+                    <div className="col-md-3">{employee.email}</div>
                 </div>
             ))}
         </div>
