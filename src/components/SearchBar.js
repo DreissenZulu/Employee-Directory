162202acdaa6da6f203b20e6075c8a8567d26a43
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-12-01 18:21:06 -0500

    Prevent user from submitting the form with Enter

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function SearchBar(props) {
   return (
-    <nav className="navbar navbar-light bg-info">
+    <nav className="navbar navbar-light bg-info" onSubmit={e => e.preventDefault()}>
       <form className="form-inline" >
         <input className="form-control mr-sm-2" name="search" onChange={props.handleSearch} type="search" placeholder="Search Directory" autoComplete="off" aria-label="Search" />
         {/* <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button> */}
