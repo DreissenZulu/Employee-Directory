af8043a64d8b670c3a60ae69938f279fe90e1680
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-11-30 20:52:26 -0500

    App shows only the employee registry

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import React from 'react';
+import EmployeeList from './components/EmployeeList'
 
 function App() {
   return (
     <div>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <EmployeeList />
     </div>
   );
 }
