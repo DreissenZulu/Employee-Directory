0b99dac63aadd8d8991bb035aaa406d9c3697b11
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-12-01 23:35:17 -0500

    Adjusted bootstrap styling, moved inline styles to css file

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 function SearchBar(props) {
   return (
-    <nav className="navbar navbar-light bg-info" onSubmit={e => e.preventDefault()}>
+    <nav className="navbar navbar-light bg-info d-flex justify-content-center position-fixed"  onSubmit={e => e.preventDefault()}>
       <form className="form-inline" >
-        <input className="form-control mr-sm-2" name="search" onChange={props.handleSearch} type="search" placeholder="Search Directory" autoComplete="off" aria-label="Search" />
+        <input className="form-control" style={{ width: "500px" }} name="search" onChange={props.handleSearch} type="search" placeholder="Search Directory" autoComplete="off" aria-label="Search" />
         {/* <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button> */}
       </form>
     </nav>
