d2bf4387e0b1aec4b29229dd549ba3522b2090ee
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-12-03 21:37:53 -0500

    Added a clear search bar method. Added to props of search bar

--- src/App.js
+++ src/App.js
@@ -9,11 +9,19 @@ function App() {
     event.preventDefault();
     setQuery(event.target.value.trim());
   }
+
+  function clearSearch (event) {
+    if (event.target.parentElement.search) {
+      event.target.parentElement.search.value = "";
+      setQuery("");
+    }
+  }
   
   return (
     <div>
       <SearchBar 
         handleSearch={handleSearch}
+        clearSearch={clearSearch}
       />
       <EmployeeList query={query} />
     </div>
