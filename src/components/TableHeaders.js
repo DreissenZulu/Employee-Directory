bf952a75adf5b2111c60c855a56b982492e5cf03
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-12-04 00:27:57 -0500

    Fixed class definitions to className

--- src/components/TableHeaders.js
+++ src/components/TableHeaders.js
@@ -6,12 +6,12 @@ function TableHeaders(props) {
         <div>
             <div className="row d-flex align-items-center tableHeader">
                 <div className="col-md-1"></div>
-                <button className="btn btn-sm col-md-1" onClick={props.handleBtnClick} name="fName">First Name <i class={props.sortBy === "fName" ? faArrow : ""}></i></button>
-                <button className="btn btn-sm col-md-1" onClick={props.handleBtnClick} name="lName">Last Name <i class={props.sortBy === "lName" ? faArrow : ""}></i></button>
-                <button className="btn btn-sm col-md-2" onClick={props.handleBtnClick} name="dob">Date of Birth <i class={props.sortBy === "dob" ? faArrow : ""}></i></button>
-                <button className="btn btn-sm col-md-2" onClick={props.handleBtnClick} name="city">City <i class={props.sortBy === "city" ? faArrow : ""}></i></button>
-                <button className="btn btn-sm col-md-2" onClick={props.handleBtnClick} name="phoneNum">Phone <i class={props.sortBy === "phoneNum" ? faArrow : ""}></i></button>
-                <button className="btn btn-sm col-md-3" onClick={props.handleBtnClick} name="email">Email <i class={props.sortBy === "email" ? faArrow : ""}></i></button>
+                <button className="btn btn-sm col-md-1" onClick={props.handleBtnClick} name="fName">First Name <i className={props.sortBy === "fName" ? faArrow : ""}></i></button>
+                <button className="btn btn-sm col-md-1" onClick={props.handleBtnClick} name="lName">Last Name <i className={props.sortBy === "lName" ? faArrow : ""}></i></button>
+                <button className="btn btn-sm col-md-2" onClick={props.handleBtnClick} name="dob">Date of Birth <i className={props.sortBy === "dob" ? faArrow : ""}></i></button>
+                <button className="btn btn-sm col-md-2" onClick={props.handleBtnClick} name="city">City <i className={props.sortBy === "city" ? faArrow : ""}></i></button>
+                <button className="btn btn-sm col-md-2" onClick={props.handleBtnClick} name="phoneNum">Phone <i className={props.sortBy === "phoneNum" ? faArrow : ""}></i></button>
+                <button className="btn btn-sm col-md-3" onClick={props.handleBtnClick} name="email">Email <i className={props.sortBy === "email" ? faArrow : ""}></i></button>
             </div>
         </div>
     )
