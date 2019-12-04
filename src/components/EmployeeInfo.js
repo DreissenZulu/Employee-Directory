d058b20db28bdaa3c72967897f5f5965667d45f4
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-12-03 20:33:12 -0500

    Added sorting switch cases, allowing the list to be sorted before render

--- src/components/EmployeeInfo.js
+++ src/components/EmployeeInfo.js
@@ -1,9 +1,33 @@
 import React from "react";
 
 function EmployeeInfo(props) {
+    let sortOrder = (props.order === "descending" ? 1 : -1)
+    let employeeList = props.employeeList
+    switch (props.sortBy) {
+        case ("fName"):
+            employeeList = employeeList.sort((a, b) => (a.name.first > b.name.first) ? sortOrder : -sortOrder);
+            break;
+        case ("lName"):
+            employeeList = employeeList.sort((a, b) => (a.name.last > b.name.last) ? sortOrder : -sortOrder);
+            break;
+        case ("dob"):
+            employeeList = employeeList.sort((a, b) => (a.dob.date < b.dob.date) ? sortOrder : -sortOrder);
+            break;
+        case ("city"):
+            employeeList = employeeList.sort((a, b) => (a.location.city > b.location.city) ? sortOrder : -sortOrder);
+            break;
+        case ("phoneNum"):
+            employeeList = employeeList.sort((a, b) => (a.phone > b.phone) ? sortOrder : -sortOrder);
+            break;
+        case ("email"):
+            employeeList = employeeList.sort((a, b) => (a.email > b.email) ? sortOrder : -sortOrder);
+            break;
+        default:
+            break;
+    }
     return (
         <div className="container">
-            {props.employeeList.map(employee => (
+            {employeeList.map(employee => (
                 <div className="row d-flex align-items-center">
                     <div className="col-md-1"><img src={employee.picture.medium} alt="Emp Img" /></div>
                     <div className="col-md-1">{employee.name.first}</div>
